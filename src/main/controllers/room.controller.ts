525251b808aff9bbda68e343a6b1d5ee1fe3d43c
Paul Henri LARBAT <paul-henri.larbat@apside-groupe.com>
2023-08-11 17:42:42 +0200

    Configuration du serveur + routes

--- src/main/controllers/room.controller.ts
+++ src/main/controllers/room.controller.ts
@@ -1,7 +1,13 @@
 import { Application } from "express";
+import { Request, Response } from "express";
+
 
 export function initRoomCtrl (server: Application) {
     server.get("/rooms", (req, res) => {
         res.end('GET /rooms');
-      });
+    });
+}
+
+export function roomWorks(req: Request, res: Response): Response {
+  return res.json({ message: "room works !" });
 }
